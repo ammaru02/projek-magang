562f9872b6fc370b7b03d60f190016233b29244a
Ammaru02 <Ammaru02@gmail.com>
2024-04-18 11:29:25 +0800

    backend new

--- client/src/components/banner/Banner.js
+++ client/src/components/banner/Banner.js
@@ -3,11 +3,6 @@ import React from "react";
 import './Banner.css';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import image1 from '../image/Banner1.jpeg'
-import image2 from '../image/Banner2.jpg'
-import image3 from '../image/Banner3.jpg'
-import image4 from '../image/Banner4.jpg'
-import image5 from '../image/Banner5.jpg'
 
 
 const Banner = ()=>{
@@ -15,21 +10,21 @@ const Banner = ()=>{
         <>
         <Carousel className="main-carousel" showThumbs={false}>
                 <div>
-                    <img src={image1} height="550px" width="200px" />
+                    <img src={image1} height="500px" width="200px" />
                     
                 </div>
                 <div>
-                    <img src={image2} height="550px" width="200px" />
+                    <img src={image2} height="500px" width="200px" />
                     
                 </div>
                 <div>
-                    <img src={image3} height="550px" width="200px" />
+                    <img src={image3} height="500px" width="200px" />
                 </div>
                 <div>
-                    <img src={image4} height="550px" width="200px" />
+                    <img src={image4} height="500px" width="200px" />
                 </div>
                 <div>
-                    <img src={image5} height="550px" width="200px" />
+                    <img src={image5} height="500px" width="200px" />
                 </div>
             </Carousel>
         </>
