27190725f7a43795f8d270d24fc7d5ae4eae352f
Ammaru02 <Ammaru02@gmail.com>
2024-06-26 11:39:58 +0800

    tampilan banner dan artikel

--- client/src/components/banner/Banner.js
+++ client/src/components/banner/Banner.js
@@ -5,15 +5,16 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
-    const [imageUrls, setImageUrls] = useState([]);
+    const [banners, setBanners] = useState([]);
 
     useEffect(() => {
-        const fetchBannerUrls = async () => {
+        const fetchBanners = async () => {
             try {
                 const response = await fetch("http://127.0.0.1:5000/banner");
                 if (response.ok) {
                     const result = await response.json();
-                    setImageUrls(result.data.map(banner => banner.url_gambar)); // Assuming the JSON response has a 'data' field with banner URLs
+                    const sortedBanners = result.data.sort((a, b) => a.id - b.id); // Sort banners by id
+                    setBanners(sortedBanners);
                 } else {
                     console.error("Failed to fetch banners:", response.statusText);
                 }
@@ -22,14 +23,14 @@ const Banner = () => {
             }
         };
 
-        fetchBannerUrls();
+        fetchBanners();
     }, []);
 
     return (
         <Carousel className="main-carousel" showThumbs={false}>
-            {imageUrls.map((imageUrl, index) => (
-                <div key={index}>
-                    <img src={imageUrl} height="500px" width="200px" />
+            {banners.map((banner) => (
+                <div key={banner.id}>
+                    <img src={banner.url_gambar} height="500px" width="200px" />
                 </div>
             ))}
         </Carousel>
